e494791650a58db416e65763c6a695f0ca51ed11
Umesh <up9158256325@gmail.com>
2023-04-28 14:26:35 +0530

    new project added

---
+++ src/com/imobdev/tcs/ArrayImpl.java
@@ -0,0 +1,10 @@
+package com.imobdev.tcs;
+
+public class ArrayImpl {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("umesh");
+	}
+
+}
---
+++ src/com/imobdev/tcs/SetImpl.java
@@ -0,0 +1,9 @@
+package com.imobdev.tcs;
+
+public class SetImpl {
+	
+	public static void main(String[] args) {
+		System.out.println("Rajesh");
+	}
+
+}
